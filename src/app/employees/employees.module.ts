c0083cffcca3cb381bb5a39c8fdb1fa91e03c9a0
OlhaKuzmyn <olya.kuzmyn@gmail.com>
2023-03-26 23:22:14 +0300

    chng

--- src/app/employees/employees.module.ts
+++ src/app/employees/employees.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EmployeesRoutingModule } from './employees-routing.module';
-import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
-import { EmployeeComponent } from './components/employee/employee.component';
 import { EmployeesComponent } from './components/employees/employees.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {EmployeeService} from "./services/employee.service";
@@ -12,8 +10,6 @@ import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    EmployeeFormComponent,
-    EmployeeComponent,
     EmployeesComponent
   ],
   imports: [
